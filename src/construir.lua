b3e9656fa9e4772acb5f047913762461850157f7
Matthias Hauber <matthiashauber@googlemail.com>
2024-09-10 17:42:56 +0200

    construir: implement git-scm fetcher

--- src/construir.lua
+++ src/construir.lua
@@ -4,6 +4,10 @@ lfs = {
   root = os.getenv("CONSTRUIR_AQUI") or "."
 }
 
+local dat = function(msg)
+  print('\27[0m-- ' .. msg .. '\27[0m')
+end
+
 local wat = function(msg)
   print('\27[1;37m== ' .. msg .. '\27[0m')
 end
@@ -16,6 +20,37 @@ local ack = function(msg)
   print('\27[0;32m++\27[0m ' .. msg)
 end
 
+local run = function(cmd)
+  local fds = { stdin = {}, stdout = {}, stderr = {} }
+  fds.stdin.read, fds.stdin.write = pipe()
+  fds.stdout.read, fds.stdout.write = pipe()
+  fds.stderr.read, fds.stderr.write = pipe()
+
+  local pid = fork()
+  if pid then
+    if pid == 0 then
+      -- child
+      fds.stdin.write:close()
+      fds.stdout.read:close()
+      fds.stderr.read:close()
+
+      dup2(fds.stdin.read, 0)
+      dup2(fds.stdout.write, 1)
+      dup2(fds.stderr.write, 2)
+
+      execvp(cmd);
+    else
+      -- parent
+      fds.stdin.read:close()
+      fds.stdout.write:close()
+      fds.stderr.write:close()
+
+      status = waitpid(pid);
+      print(fds.stdout.read:read("a*"))
+    end
+  end
+end
+
 local semver = function(self)
   local v = self.version:gmatch("[^.%s]+")
   return v(), v(), v()
@@ -24,8 +59,8 @@ end
 local parse = function(target)
   local fenv = { pkg = {} }
   setmetatable(fenv, { __index = _G })
-  _ENV = fenv
 
+  _ENV = fenv
   setmetatable(pkg, {
     __index = function(self, key)
       local meta = {
@@ -41,18 +76,30 @@ local parse = function(target)
 
   local recipe = loadfile(string.format("%s/%s/%s.lua", lfs.root, lfs.recipes, target), "t", fenv)
   if not recipe then
-    nak(string.format("parsing recipe (%s) failed", target))
+    nak(string.format("\27[0;33m%s\27[0m recipe failed", target))
     return nil
   end
   recipe()
-  ack(string.format("parsing recipe (%s/%s)", target, fenv.pkg.version))
+  ack(string.format("\27[0;33m%s/%s\27[0m recipe parsed", target, fenv.pkg.version))
 
   return fenv.pkg
 end
 
+local fetch = function(pkg)
+  for _, v in pairs(pkg.scm.git or {}) do
+    dat(string.format("\27[0;33m%s/%s\27[0m fetch \27[0;34m%s\27[0m", pkg.name, pkg.version, v.remote))
+    run(string.format("git clone --bare --mirror %s %s/%s/git/%s", v.remote, lfs.root, lfs.downloads, pkg.name))
+  end
+end
+
 dofile(string.format("%s/conf/construir.lua", lfs.root))
 lfs.recipes = lfs.recipes or "recipes"
+lfs.downloads = lfs.downloads or "dl"
 
 wat("construir: a custom linux distribution of your needs")
 local target = arg[2]
 local pkg = parse(target)
+
+if pkg.scm then
+  fetch(pkg)
+end
